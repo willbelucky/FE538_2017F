8d649b8c8aca4bb9b71046d5ea6185006d67416d
gs91ting <gs91ting@hotmail.com>
2017-11-21 14:11:40 +0900

    directory first push

--- K_means/k_means.py
+++ K_means/k_means.py
@@ -1,4 +1,6 @@
 from K_means._k_means import *
+from data.test_data_reader import *
+import pandas as pd
 
 
 def get_highest_volatility_group(n, k):
@@ -11,4 +13,17 @@ def get_highest_volatility_group(n, k):
         index       code        | (string) 6 digit number string code of stock.
         columns     name        | (string) The name of company.
     """
-    pass
+    days=n
+    means = k
+    stock_masters = get_stock_master()
+    highest_volatilities = get_highest_volatility(window=days, stock_masters=stock_masters.sample(10))
+    centroids = initialize_centroids(means)
+    df = assignment(highest_volatilities, centroids, days)
+    # reassign updated means
+    centroids = update(df, centroids)
+    df = assignment(df, centroids, days)
+    highest_mean = max(centroids, key=centroids.get)
+    final_df=stock_masters[stock_masters.index.isin(np.array(df[df['closest'] == highest_mean].index))]
+    return final_df
+
+
